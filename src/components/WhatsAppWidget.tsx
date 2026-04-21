a47f386ec05b88b2173c5ef88576def47727060b
Ibrahima Guisse <iguisse97@gmail.com>
2026-04-21 20:34:05 +0000

    whatsapp widget

--- src/components/WhatsAppWidget.tsx
+++ src/components/WhatsAppWidget.tsx
@@ -16,7 +16,7 @@ const WhatsAppIcon = ({ size = 24, className = "" }: { size?: number, className?
 const WhatsAppWidget = () => {
     const [showTooltip, setShowTooltip] = useState(false);
     const isDragging = useRef(false);
-    const phoneNumber = "221786349573";
+    const phoneNumber = "221788925878";
     const message = "Bonjour ! J'aimerais en savoir plus sur Baysawaar.";
 
     const handleWhatsAppClick = () => {
